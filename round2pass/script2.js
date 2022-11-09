8a591c1a520a5b66e8e41cae9858d7a11e998bda
devitodev <softwaredev.ito@gmail.com>
2022-11-08 23:01:54 -0600

    Worked on a few small bugs

--- round2pass/script2.js
+++ round2pass/script2.js
@@ -1,5 +1,5 @@
-var allOptions = [];
 
+var allOptions = [];
 var numbers = [1,2,3,4,5,6,7,8,9,0];
 var symbols = ['!','@','#','$','%','^','&','*','(',')','{','}','[',']','=','<','>'];
 var lowercaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -8,40 +8,52 @@ var uppercaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N',
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", genPrompt);
 
-function generatePassword(passwordLength, allOptions) {
-    var password = '';
-    for(var i = 0; i < passwordLength; i++) {
+function generatePassword(passwordLength,password) {
+  let remaining = 0;
+  remaining = passwordLength - password.length;
+    for(var i = 0; i < remaining; i++) {
         var randomIndex = (Math.floor(Math.random() * allOptions.length));
         password += allOptions[randomIndex];
     }
     writePassword(password)
 }
 
+function loopCharacter (arr) {
+  var randomIndex = (Math.floor(Math.random() * arr.length));
+        return arr[randomIndex];
+}
+
 function writePassword(password) {
     var passwordText = document.querySelector("#password");
    passwordText.value = password;
+   password = "";
 }
 
 function genPrompt () {
-  passwordLength = parseInt(prompt('What is your preferred length for your password? (it must be 8-128)'))
+  var password = ""
+  let passwordLength = parseInt(prompt('What is your preferred length for your password? (it must be 8-128)'))
   
   if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Character length must be a number 8-128, my dude");
-    return false;
+    return null;
   }
   if (confirm('Include lowercase letters?')) {
     allOptions = allOptions.concat(lowercaseLetters);
+    password += loopCharacter(lowercaseLetters)
   }
   if (confirm('Include uppercase letters?')) {
     allOptions = allOptions.concat(uppercaseLetters);
+    password += loopCharacter(uppercaseLetters)
   }
   if (confirm('Include symbols')) {
     allOptions = allOptions.concat(symbols);
+    password += loopCharacter(symbols)
   }
   if (confirm('Include number?')) {
     allOptions = allOptions.concat(numbers);
+    password += loopCharacter(numbers)
   }
 
-generatePassword(passwordLength, allOptions)
+generatePassword(passwordLength,password)
 
 }
